fb3f5486b888ceca13b1281087adde721abe6d0f
KunalKapure05 <kapurekunal61@gmail.com>
2024-07-02 01:18:24 +0530

    Implemented greeting message for start command

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import { Telegraf } from "telegraf";
 import {message } from "telegraf/filters";
+import User from './src/model/User.js';
 import { config } from "dotenv";
+
+
 import connectDb from "./src/config/db.js";
 
 config();
@@ -14,16 +17,43 @@ catch{
 
 }
 
+
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.start((ctx)=>{
-    
-    ctx.reply("Welcome to the bot!")
-})
+bot.start(async (ctx)=>{
+    const from  = ctx.update.message.from;
 
+    
+    console.log('ctx',ctx.update.message.chat);
+    
+    console.log('from', from);
+
+    try{
+        await User.findOneAndUpdate({tgId:from.id},{
+            $setOnInsert:{
+                firstName:from.first_name,
+                lastName:from.last_name,
+                isBot:from.isBot
+            }
+        },{
+            upsert:true,
+            new:true
+        })
+
+        await ctx.reply(`Hey ${from.first_name}! 👋 I'm Interacta-Bot🤖, your interactive chatbot companion. I'm here to make your Telegram experience more interactive and fun! Whether you need quick answers, assistance with tasks, or just want to chat, I've got you covered.`)
+    
+    }
+        catch(error){
+            console.error('error in the/start command: ',error);
+            await ctx.reply('facing some difficulties to start the bot')
+
+        }
+})
 bot.on(message('text'), async (ctx) => {
+
+
   
-    await ctx.reply(`Hello ${ctx.state.role}`)
+    
   })
 
 
